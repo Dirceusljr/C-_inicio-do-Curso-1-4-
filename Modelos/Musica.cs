967a077a3ac40974b8c377a20b427e38cd0a4fdf
Dirceu dos Santos <141691213+Dirceusljr@users.noreply.github.com>
2023-08-20 15:19:22 -0300

    Aulas do Curso de C#
    Aulas referentes ao módulos 1-4 de C# da Alura:
    -C#: criando sua primeira aplicação
    -C#: aplicando a Orientação a Objetos
    -C#: dominando Orientação a Objetos
    -C#: consumindo API, gravando arquivos e utilizando o LINQ

---
+++ Modelos/Musica.cs
@@ -0,0 +1,46 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ScreenSound_04.Modelos;
+
+internal class Musica
+{
+    private List<string> tonalidades = new List<string> { "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B" };
+
+    [JsonPropertyName("song")]
+    public string? Nome { get; set; }
+    [JsonPropertyName("artist")]
+    public string? Artista { get; set; }
+
+    [JsonPropertyName("duration_ms")]
+    public int Duracao { get; set; }
+    [JsonPropertyName("genre")]
+    public string? Genero { get; set; }
+
+    [JsonPropertyName("key")]
+    public int Key { get; set; }
+
+    public string Tonalidade {
+        get
+        {
+            return tonalidades[Key];
+        }
+    }
+    public void ExibirDetalhesDaMusica()
+    {
+        Console.WriteLine($"Artista: {Artista}");
+        Console.WriteLine($"Música: {Nome}");
+        Console.WriteLine($"Duração em segundos: {Duracao /1000}");
+        Console.WriteLine($"Gênero musical: {Genero}");
+        Console.WriteLine($"Tonalidade: {Tonalidade}");
+    }
+
+    public static void MostrarListaDeTonalidades()
+    {
+        List<string> tonalidades = new List<string> { "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B" };
+        foreach (var indice in tonalidades)
+        {
+            Console.WriteLine($"Para a tonalidade {tonalidades}, aperte {indice}.");
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using ScreenSound_04.Modelos;
+using System.Text.Json;
+using ScreenSound_04.Filtros;
+
+using (HttpClient client = new HttpClient())
+{
+    try
+    {
+        string resposta = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
+        var musicas = JsonSerializer.Deserialize<List<Musica>>(resposta)!;
+
+
+        //musicas[1].ExibirDetalhesDaMusica();
+        //LinqFilter.FiltrarTodosOsGenerosMusicais(musicas);
+        //LinqOrder.ExibirListaDeArtistasOrdenados(musicas);
+        //LinqFilter.FiltrarArtistasPorGeneroMusical(musicas, "rock");
+        //LinqFilter.FiltrarMusicasDeUmArtista(musicas, "U2");
+        LinqFilter.FiltrarMusicasPorTonalidade(musicas);
+
+        //var musicasFavoritasDirceu = new MusicasPreferidas("Dirceu");
+        //musicasFavoritasDirceu.AdicionarMusicasFavoritas(musicas[537]);
+        //musicasFavoritasDirceu.AdicionarMusicasFavoritas(musicas[238]);
+        //musicasFavoritasDirceu.AdicionarMusicasFavoritas(musicas[761]);
+        //musicasFavoritasDirceu.AdicionarMusicasFavoritas(musicas[970]);
+        //musicasFavoritasDirceu.AdicionarMusicasFavoritas(musicas[489]);
+
+        //musicasFavoritasDirceu.ExibirMusicasFavoritas();
+
+        //musicasFavoritasDirceu.GerarArquivoJson();
+
+
+
+
+
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Temos um problema: {ex.Message}");
+    }
+}
